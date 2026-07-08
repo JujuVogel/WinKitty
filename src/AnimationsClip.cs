8b3112786b510c728a25c2929dda96f14e052b87
JujuVogel <vogeljulian131104@gmail.com>
2026-07-08 20:50:59 +0200

    added grabbing animation

--- src/AnimationsClip.cs
+++ src/AnimationsClip.cs
@@ -4,14 +4,14 @@ public record AnimationClip(string Path, int FrameWidth, int FrameHeight, int Fr
 
 public static class Animations
 {
-    public static readonly AnimationClip Idle = new("assets/cat_animations/Idle.png", 64, 32, 4, 6, true);
-    public static readonly AnimationClip Idle2 = new("assets/cat_animations/Idle_2.png", 64, 32, 4, 6, true);
-    public static readonly AnimationClip Running = new("assets/cat_animations/Running.png", 64, 32, 6, 10, true);
-    public static readonly AnimationClip Jumping = new("assets/cat_animations/Jumping.png", 64, 32, 6, 10, false);
-    public static readonly AnimationClip Sleeping = new("assets/cat_animations/Sleeping.png", 64, 32, 4, 4, true);
-    public static readonly AnimationClip Stretching = new("assets/cat_animations/Stretching.png", 64, 32, 4, 6, false);
-    public static readonly AnimationClip Cleaning = new("assets/cat_animations/Cleaning.png", 64, 32, 4, 6, true);
-    public static readonly AnimationClip Eating = new("assets/cat_animations/Eating.png", 64, 32, 4, 6, true);
-    public static readonly AnimationClip Play = new("assets/cat_animations/Play.png", 64, 32, 4, 8, true);
-    public static readonly AnimationClip Attack = new("assets/cat_animations/Attack.png", 64, 32, 4, 10, false);
+    public static readonly AnimationClip Idle = new("assets/cat_animations/Idle.png", 32, 32, 4, 1000 / 150, true);
+    public static readonly AnimationClip Idle2 = new("assets/cat_animations/Idle_2.png", 32, 32, 4, 1000 / 150, true);
+    public static readonly AnimationClip Running = new("assets/cat_animations/Running.png", 32, 32, 8, 1000 / 80, true);
+    public static readonly AnimationClip Sleeping = new("assets/cat_animations/Sleeping.png", 32, 32, 4, 1000 / 200, true);
+    public static readonly AnimationClip Jumping = new("assets/cat_animations/Jumping.png", 32, 32, 8, 1000 / 90, false);
+    public static readonly AnimationClip Stretching = new("assets/cat_animations/Stretching.png", 32, 32, 8, 1000 / 120, false);
+    public static readonly AnimationClip Eating = new("assets/cat_animations/Eating.png", 32, 32, 4, 1000 / 130, true);
+    public static readonly AnimationClip Cleaning = new("assets/cat_animations/Cleaning.png", 32, 32, 4, 1000 / 110, true);
+    public static readonly AnimationClip Play = new("assets/cat_animations/Play.png", 32, 32, 6, 1000 / 100, true);
+    public static readonly AnimationClip Attack = new("assets/cat_animations/Attack.png", 32, 32, 7, 1000 / 85, false);
 }
