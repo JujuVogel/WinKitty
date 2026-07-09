7a1d2090f833ccef8a92cf9175d5821c8cc2c0a8
JujuVogel <vogeljulian131104@gmail.com>
2026-07-09 23:14:04 +0200

    added desktop only option

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -9,4 +9,11 @@ internal static class NativeMethods
     [DllImport("user32.dll")] public static extern int SetWindowLong(IntPtr hWnd, int index, int newStyle);
     public const int GWL_EXSTYLE = -20;
     public const int WS_EX_TOOLWINDOW = 0x80;
+    [DllImport("user32.dll")] public static extern IntPtr FindWindow(string cls, string? win);
+    [DllImport("user32.dll")] public static extern IntPtr FindWindowEx(IntPtr parent, IntPtr childAfter, string cls, string? win);
+    [DllImport("user32.dll")] public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam, uint flags, uint timeout, out IntPtr result);
+    [DllImport("user32.dll")] public static extern bool EnumWindows(EnumWindowsProc cb, IntPtr lParam);
+    [DllImport("user32.dll")] public static extern IntPtr SetParent(IntPtr child, IntPtr newParent);
+    [DllImport("user32.dll")] public static extern IntPtr GetDesktopWindow();
+    public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
 }
